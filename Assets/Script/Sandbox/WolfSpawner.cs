3f5ea0ef509aab26c4046cf00f8ba54e48a66349
Diego Contreras <smarttdiego@gmail.com>
2020-10-13 17:04:38 -0700

    Added Wolf Demo

---
+++ Assets/Script/Sandbox/WolfSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class WolfSpawner : MonoBehaviour
+{
+    float timer = 0.0f;
+    float delay = 2.0f;
+
+    public void Awake()
+    {
+        timer = Time.time + delay;
+    }
+
+    public void Update()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            if (Time.time > timer)
+            {
+                timer = Time.time + delay;
+                BoltNetwork.Instantiate(BoltPrefabs.Wolf, Vector3.zero, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Sandbox/WolfTestScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class WolfTestScript : Bolt.EntityBehaviour<IMobState>
+{
+    public float range = 2.0f;
+    public float speed = 3.0f;
+
+    public override void Attached()
+    {
+        state.SetTransforms(state.PositionTransform, transform);
+    }
+
+    public override void SimulateOwner()
+    {
+        foreach(var player in PlayerRegistry.AllPlayers)
+        {
+            Vector3 position = player.character.gameObject.transform.position;
+            Vector3 difference = position - transform.position;
+
+            if(difference.sqrMagnitude < Mathf.Pow(range, 2))
+            {
+                transform.position += difference.normalized * speed * BoltNetwork.FrameDeltaTime;
+                break; 
+            }
+
+        }
+    }
+}
