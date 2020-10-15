79f939968bfdb0a94d379c4048bd4b0ac25afba0
Diego Contreras <smarttdiego@gmail.com>
2020-10-14 20:59:36 -0700

    Wolves are now pushable

--- Assets/Script/Sandbox/WolfTestScript.cs
+++ Assets/Script/Sandbox/WolfTestScript.cs
@@ -7,13 +7,38 @@ public class WolfTestScript : Bolt.EntityBehaviour<IMobState>
 {
     public float range = 2.0f;
     public float speed = 3.0f;
+    public BoxCollider2D boxCollider;
+    public CollisionMarker collisionMarker;
+
+    private void CalculateCollidedPosition()
+    {
+        Collider2D[] colliderHits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0.0f);
+
+        foreach (Collider2D hit in colliderHits)
+        {
+            if (hit == boxCollider) continue;
+
+            CollisionMarker hitMarker = hit.GetComponent<CollisionMarker>();
+
+            if (collisionMarker.IsPushed(hitMarker.CollisionNumber))
+            {
+                ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+                if (colliderDistance.isOverlapped)
+                {
+                    transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+                }
+            }
+        }
+    }
+
 
     public override void Attached()
     {
         state.SetTransforms(state.PositionTransform, transform);
     }
 
-    public override void SimulateOwner()
+    public void Update()
     {
         foreach(var player in PlayerRegistry.AllPlayers)
         {
@@ -25,7 +50,7 @@ public class WolfTestScript : Bolt.EntityBehaviour<IMobState>
                 transform.position += difference.normalized * speed * BoltNetwork.FrameDeltaTime;
                 break; 
             }
-
         }
+        CalculateCollidedPosition();
     }
 }
