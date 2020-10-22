b07c2e89a1df755cd89782aec81e58d36566e566
Diego Contreras <smarttdiego@gmail.com>
2020-10-22 16:33:02 -0700

    Refactored Game/ChatboxMessageManager.cs

--- Assets/Script/Game/ChatboxMessageManager.cs
+++ Assets/Script/Game/ChatboxMessageManager.cs
@@ -1,49 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 /// <summary>
-/// A class that deals with requesting and viewing chatbox messages. 
+/// A class that deals with requesting and viewing chatbox messages. Must be put
+/// into the "Content" gameObject of the chatbox.
 /// </summary>
 public class ChatboxMessageManager : Bolt.GlobalEventListener
 {
-    private GameObject chatTextPrefab;
-
-    [SerializeField]
-    private InputFieldWrapper inputFieldWrapper;
-
-    [SerializeField]
-    private Transform content;
-
+    private GameObject chatTextPrefab = null;
 
     private void Awake()
     {
         chatTextPrefab = (GameObject)Resources.Load("ChatText");
-        inputFieldWrapper.onReturn.AddListener(SendBoltMessage);
     }
 
-    private void SendBoltMessage(string message)
+    /// <summary>
+    /// Raise a ChatMessageRequestEvent containing a chat messsage to the server. 
+    /// This is usually attached to an input field.
+    /// </summary>
+    public void RequestMessage(string message)
     {
         ChatMessageRequestEvent chatMessageEvent = ChatMessageRequestEvent.Create();
         chatMessageEvent.Message = message;
         chatMessageEvent.Send();
     }
 
-    private void CreateMessage(string message)
+    /// <summary>
+    /// Creates a text prefab as a child of this gameObject for it to
+    /// be seen in chat.
+    /// </summary>
+    private void InstantiateTextPrefab(string message)
     {
-        Message newMessage = new Message();
-        newMessage.message = message;
-
-        GameObject newTextMessage = GameObject.Instantiate(chatTextPrefab, content);
+        GameObject newTextMessage = GameObject.Instantiate(chatTextPrefab, transform);
         TextMeshProUGUI TMPtext = newTextMessage.GetComponent<TextMeshProUGUI>();
-
         TMPtext.text = message;
     }
 
 
     public override void OnEvent(ChatMessageEvent evnt)
     {
-        CreateMessage(evnt.Message);
+        // Display any and all messages approved by server.
+        InstantiateTextPrefab(evnt.Message);
     }
 }
