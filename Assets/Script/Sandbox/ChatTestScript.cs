74c8fea085de5ce3ac00c4b9d2e77c4a2efc459c
Diego Contreras <smarttdiego@gmail.com>
2020-10-21 19:50:27 -0700

    Chatbox can send messages locally

--- Assets/Script/Sandbox/ChatTestScript.cs
+++ Assets/Script/Sandbox/ChatTestScript.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+public class Message
+{
+    public string message;
+}
+
 
 public class ChatTestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<Message> messages = new List<Message>();
+
+    private GameObject chatTextPrefab;
+
+
+    [SerializeField]
+    private InputFieldWrapper inputFieldWrapper;
+
+    [SerializeField]
+    private Transform content;
+
+
+    private void Awake()
     {
-        
+        chatTextPrefab = (GameObject)Resources.Load("ChatText");
+        inputFieldWrapper.onReturn.AddListener(CreateMessage);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateMessage(string message)
     {
-        
+        Message newMessage = new Message();
+        newMessage.message = message;
+
+        messages.Add(newMessage);
+
+        GameObject newTextMessage = GameObject.Instantiate(chatTextPrefab, content);
+        TextMeshProUGUI TMPtext = newTextMessage.GetComponent<TextMeshProUGUI>();
+
+        TMPtext.text = message;
     }
+
+
+
 }
