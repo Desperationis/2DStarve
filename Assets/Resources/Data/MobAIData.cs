9b6c32cf2e7897bb8e0f106eb3d8a59196194187
Diego Contreras <smarttdiego@gmail.com>
2021-03-21 22:20:46 -0700

    Add separate MovementData and AI data to Rabbit and Bat
    Before this commit, the Rabbit and Bat prefabs used the wolf's AI data to operate. This is obviously not good for production, so this commit will change that.

--- Assets/Resources/Data/MobAIData.cs
+++ Assets/Resources/Data/MobAIData.cs
@@ -3,10 +3,19 @@
 [CreateAssetMenu(fileName = "New Mob Data", menuName = "MobAIData")]
 public class MobAIData : ScriptableObject
 {
-    // AI data.
-    public float followerRange;
-    public bool runAtFollow;
-    public float attackRange;
+    [Header("Idle AI")]
+    [Tooltip("Number of seconds that this mob will rest for before moving.")]
     public float idleRestTime;
+    [Tooltip("Number of seconds that this mob will move for before resting.")]
     public float idleMoveTime;
+
+    [Header("Follower AI")]
+    [Tooltip("The radius used to search for players in unity units.")]
+    public float followerRange;
+    [Tooltip("Whether or not to run at a player when following them")]
+    public bool runAtFollow;
+
+    [Header("Attack AI")]
+    [Tooltip("The radius used to search for players in unity units.")]
+    public float attackRange;
 }
