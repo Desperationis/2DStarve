aa997bc2f739a346a45fc4b3e820868606a4c6d7
Diego Contreras <smarttdiego@gmail.com>
2021-03-23 12:41:44 -0700

    Add MapEntityLoader.cs
    Add MapEntityLoader.cs to dynamically add mobs according to object layers in Tiled. This works by using the names of the layers to set the type of the mob, and uses the x-y coords of each object in that layer to spawn a mob.

---
+++ Assets/Scripts/Game/Server/MapEntityLoader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using SuperTiled2Unity;
+using Bolt;
+
+/// <summary>
+/// Dynamically instantiates the entities of a Tiled Map.
+/// </summary>
+public class MapEntityLoader : MonoBehaviour
+{
+    [SerializeField]
+    private SuperMap map = null;
+
+    /// <summary>
+    /// Loads all entities of a specific layer
+    /// </summary>
+    /// <param name="layer">SuperTiled2Unity Layer</param>
+    /// <param name="mob">PrefabId of the mob</param>
+    private void LoadLayer(SuperObjectLayer layer, PrefabId mob)
+    {
+        SuperObject[] entities = layer.GetComponentsInChildren<SuperObject>();
+
+        foreach (SuperObject entity in entities)
+        {
+            // Spawn mobs relative to map
+            Vector3 position = new Vector3(entity.m_X / map.m_TileWidth, entity.m_Y / map.m_TileHeight);
+            position += map.transform.position;
+
+            BoltNetwork.Instantiate(mob, position, Quaternion.identity);
+        }
+    }
+
+
+
+    void Start()
+    {
+        // Only spawn prefabs server-side
+        if(BoltNetwork.IsServer)
+        {
+            SuperObjectLayer[] layers = map.GetComponentsInChildren<SuperObjectLayer>();
+            foreach(SuperObjectLayer layer in layers)
+            {
+                switch (layer.m_TiledName)
+                {
+                    case "Wolf":
+                        LoadLayer(layer, BoltPrefabs.Wolf);
+                        break;
+                    case "ForestBat":
+                        LoadLayer(layer, BoltPrefabs.ForestBat);
+                        break;
+                    case "Rabbit":
+                        LoadLayer(layer, BoltPrefabs.Rabbit);
+                        break;
+                }
+
+            }
+        }
+    }
+}
