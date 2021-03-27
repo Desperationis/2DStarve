7caeeaf34caf5c506260ca4ee1858c577b2ea097
Diego Contreras <smarttdiego@gmail.com>
2021-03-26 17:47:45 -0700

    Added a tshirt
    This shirt is a shirt that is overlayed on top of the player as a proof of concept for armor and weapon sync. Here's how it was done:
    
    1. A Sprite group was attached to the parent so that any child renderers would still get covered behind trees. As a result, healthbar is also covered
    2. The .meta file of the player was copy + pasted into the shirt as it needed the exact same frames and anchor points as the player
    3. A custom script syncs the shirt animation with the player via sprite names so that no other animations are necessary at the expense at pre-loading sprites in a separate script.

---
+++ Assets/Scripts/Sandbox/ShirtSync.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirtSync : MonoBehaviour
+{
+    public SpriteRenderer parent = null;
+    public SpriteRenderer thisThing = null;
+    private Dictionary<string, Sprite> spritesheet = new Dictionary<string, Sprite>();
+
+    // Update is called once per frame
+    void Awake()
+    {
+        foreach(var sprite in Resources.LoadAll<Sprite>("Spritesheets/Shirt"))
+        {
+            spritesheet[sprite.name] = sprite;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        var sprite = spritesheet[parent.sprite.name];
+        thisThing.sprite = sprite;
+        thisThing.flipX = parent.flipX;
+    }
+}
