09c783f088532969f2c362cc59815a3d529cad43
Diego Contreras <smarttdiego@gmail.com>
2021-03-26 23:18:18 -0700

    Add synchronized day/night cycle
    Using a sin wave, linearly interpolate between night and day. Then sync this value to all clients.

---
+++ Assets/Scripts/Game/Common/DayTimeProgressor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sends events to progress the day.
+/// </summary>
+public class DayTimeProgressor : MonoBehaviour
+{
+    private Color32 day = new Color32(255, 255, 255, 255);
+    private Color32 night = new Color32(225, 201, 255, 255);
+
+    float sendTimer = 0.0f;
+
+    private void ChangeDaylightColor(Color32 color)
+    {
+        if (BoltNetwork.IsServer)
+        {
+            DayTimeChange evnt = DayTimeChange.Create();
+            evnt.color = color;
+            evnt.Send();
+        }
+    }
+
+    private void Update()
+    {
+        if(BoltNetwork.IsServer)
+        {
+            if(Time.realtimeSinceStartup > sendTimer) {
+
+                // 300 seconds night->day and 300 seconds day->night
+                float x = Time.realtimeSinceStartup;
+                float t = (Mathf.Sin((x / 95.4f) - (Mathf.PI / 2.0f)) / 2.0f) + .5f;
+                Color32 intermediate = Color32.Lerp(day, night, t);
+
+                ChangeDaylightColor(intermediate);
+                sendTimer = Time.realtimeSinceStartup + 1.0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Common/Network/DayTimeOrchestrator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Syncs daylight color based on server events
+/// </summary>
+public class DayTimeOrchestrator : Bolt.GlobalEventListener
+{
+    [SerializeField]
+    private new SpriteRenderer light = null;
+
+    public override void OnEvent(DayTimeChange evnt)
+    {
+        light.color = evnt.color;
+    }
+}
