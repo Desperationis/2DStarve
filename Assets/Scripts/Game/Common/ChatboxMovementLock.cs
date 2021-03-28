e85f7d67aeddbb4dff88c9fbb5cbf8de446e4526
Diego Contreras <smarttdiego@gmail.com>
2021-03-28 10:42:22 -0700

    Rename ChatPlayerMovementLock.cs to ChatboxMovementLock.cs
    There's no need to have "Player" in the name of this script as it's a UI script, meaning that there is an underlying assumption that only the player can see the chatbox.
    
    By renaming it to ChatboxMovementLock.cs, it makes it more clear that the script it in the chatbox.

--- Assets/Scripts/Game/Common/ChatboxMovementLock.cs
+++ Assets/Scripts/Game/Common/ChatboxMovementLock.cs
@@ -4,7 +4,7 @@
 /// Locks the currently controlled player in position while typing by completely
 /// disabling the player from sending input commands.
 /// </summary>
-public class ChatPlayerMovementLock : MonoBehaviour
+public class ChatboxMovementLock : MonoBehaviour
 {
     /// Functions below should ONLY go below OnSelect and OnDeselect. Unity's
     /// stupid inputField won't let me do this on Awake() as there are no events
