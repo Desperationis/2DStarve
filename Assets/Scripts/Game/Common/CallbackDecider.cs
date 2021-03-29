1f7597e9ff8580cdbc5c77e7a3866a5d046e88bd
Diego Contreras <smarttdiego@gmail.com>
2021-03-29 15:57:46 -0700

    Fix ServerCallbacks.cs error
    In clients, ServerCallbacks.cs was called. To fix this, set both ServerCallbacks.cs and ClientCallbacks.cs to disabled, and only enable the correct one with callback decider.

---
+++ Assets/Scripts/Game/Common/CallbackDecider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Decides which callback is enabled depending on
+/// the current mode.
+/// </summary>
+public class CallbackDecider : MonoBehaviour
+{
+    [SerializeField]
+    private ServerCallbacks serverCallback = null;
+    
+    [SerializeField]
+    private ClientCallbacks clientCallback = null;
+
+    private void Awake()
+    {
+        if(BoltNetwork.IsServer)
+        {
+            serverCallback.enabled = true;
+        }
+        if (BoltNetwork.IsClient)
+        {
+            clientCallback.enabled = true;
+        }
+    }
+}
