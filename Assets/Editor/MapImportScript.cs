449df48e7cdca645b921235430edb9453a85b0e8
Diego Contreras <smarttdiego@gmail.com>
2021-03-29 20:51:56 -0700

    Add Custom Map Import and Populated Map
    Added a custom Tiled map import that automatically set the tiles to the background layer, as well as populated the map with trees, wolves, rabbits, campfires, and bats

---
+++ Assets/Editor/MapImportScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using SuperTiled2Unity;
+
+#if UNITY_EDITOR
+using SuperTiled2Unity.Editor;
+public class MapImportScript : CustomTmxImporter
+{
+    public override void TmxAssetImported(TmxAssetImportedArgs args)
+    {
+        // Simply set the layer to background
+        SuperMap map = args.ImportedSuperMap;
+        TilemapRenderer renderer = map.GetComponentInChildren<TilemapRenderer>();
+        renderer.sortingLayerName = "Background";
+    }
+}
+#endif
